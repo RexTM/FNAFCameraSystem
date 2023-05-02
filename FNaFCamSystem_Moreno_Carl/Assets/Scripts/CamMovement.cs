74fcf5802155f6f53040af76fc20a9410380d3d4
Rex <mitchellmoreno7@gmail.com>
2023-05-02 02:27:34 -0700

    Created new fresh new script for camera movement for the Camera System
    Script now responds when the mouse is moved on the left or right hand sides of the screen.

--- FNaFCamSystem_Moreno_Carl/Assets/Scripts/CamMovement.cs
+++ FNaFCamSystem_Moreno_Carl/Assets/Scripts/CamMovement.cs
@@ -4,15 +4,91 @@ using UnityEngine;
 
 public class CamMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Variables of the Camera Movement
+    public Transform playerCam;
+    public int rotateSpeed, clampValue;
+    public bool ADKeys, movingLeft, movingRight;
+    public int targetFramerate;
+
+    //Creates the frame rate
     void Start()
     {
-        
+        Application.targetFrameRate = targetFramerate;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //If ADKeys is == true, you may rotate the camera left and right with A & D.
+        if (ADKeys == true)
+        {
+            //When you press the A key, the camera will rotate left.
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (playerCam.localRotation == Quaternion.Euler(0, -clampValue, 0))
+                {
+
+                }
+                else
+                {
+                    playerCam.Rotate(0, -rotateSpeed, 0);
+                }
+            }
+            //When you press the D key, the camera will rotate right.
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (playerCam.localRotation == Quaternion.Euler(0, clampValue, 0))
+                {
+
+                }
+                else
+                {
+                    playerCam.Rotate(0, rotateSpeed, 0);
+                }
+            }
+        }
+        //If ADKeys == false, the regular camera rotation will be available.
+        if (ADKeys == false)
+        {
+            //If either movingLeft or movingRight is == true, the camera will rotate left or right.
+            if (movingLeft == true)
+            {
+                if (playerCam.localRotation == Quaternion.Euler(0, -clampValue, 0))
+                {
+
+                }
+                else
+                {
+                    playerCam.Rotate(0, -rotateSpeed, 0);
+                }
+            }
+            if (movingRight == true)
+            {
+                if (playerCam.localRotation == Quaternion.Euler(0, clampValue, 0))
+                {
+
+                }
+                else
+                {
+                    playerCam.Rotate(0, rotateSpeed, 0);
+                }
+            }
+        }
+    }
+    //This will set the movement for the mouse controls
+    public void rotateLeft()
+    {
+        movingLeft = true;
+    }
+    public void rotateRight()
+    {
+        movingRight = true;
+    }
+    public void stopRotateLeft()
+    {
+        movingLeft = false;
+    }
+    public void stopRotateRight()
+    {
+        movingRight = false;
     }
 }
